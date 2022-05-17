6d2a4e3b95d0585962f0b80c20fcb19cbb049103
221B Baker Street <thomasshelbyn15@gmail.com>
2022-05-17 10:46:40 +0700

    Deploy website

---
+++ app.py
@@ -0,0 +1,7 @@
+from flask import Flask , render_template , url_for
+app = Flask(__name__, static_url_path='/static')
+@app.route("/")
+def home():
+    return render_template('index.html')
+if __name__ == '__main__':   
+    app.run(debug=True)
